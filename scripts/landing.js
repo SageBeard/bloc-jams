0e320de621be1800faa66d69abf9239fba82ad87
SageEnvisions <sagebeard@Sages-MacBook-Pro.local>
2016-10-15 11:02:39 -0700

    fixed chkpt 18

--- scripts/landing.js
+++ scripts/landing.js
@@ -13,11 +13,10 @@ $(window).load(function() {
     if ($(window).height() > 950) {
         animatePoints();
      }
+      var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
       $(window).scroll(function(event) {
-        var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
-          
-         if ($(window).scrollTop() >= scrollDistance) {
-             animatePoints();  
+       if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
          }
      });
- });
+    });
